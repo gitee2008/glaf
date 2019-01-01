6fba1f2ef690c211483d0fd75c68067737b5bdd6
jior2008 <jior2008@163.com>
2019-01-01 19:46:19 +0800

    update

--- src/main/java/com/glaf/core/util/UUID32.java
+++ src/main/java/com/glaf/core/util/UUID32.java
@@ -18,10 +18,27 @@
 
 package com.glaf.core.util;
 
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 public class UUID32 {
+
+	public static String[] chars = new String[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+			"o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8",
+			"9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+			"U", "V", "W", "X", "Y", "Z" };
+
+	public static String generateShortUuid() {
+		StringBuffer shortBuffer = new StringBuffer();
+		String uuid = UUID.randomUUID().toString().replace("-", "");
+		for (int i = 0; i < 8; i++) {
+			String str = uuid.substring(i * 4, i * 4 + 4);
+			int x = Integer.parseInt(str, 16);
+			shortBuffer.append(chars[x % 0x3E]);
+		}
+		return shortBuffer.toString();
+	}
 
 	public static String getUUID() {
 		String uuid = UUID.randomUUID().toString();
@@ -30,13 +47,12 @@ public class UUID32 {
 	}
 
 	public static void main(String[] args) {
-		Map<String, Object> map = new java.util.HashMap<String, Object>();
-		for (int i = 0; i < 10000; i++) {
-			String id = UUID32.getUUID();
-			System.out.println(id);
-			map.put(id, id);
+		Set<String> set = new HashSet<String>();
+		for (int i = 0; i < 1000000; i++) {
+			set.add(generateShortUuid());
+			// System.out.println(generateShortUuid());
 		}
-		System.out.println(map.size());
+		System.out.println(set.size());
 	}
 
 	private UUID32() {
